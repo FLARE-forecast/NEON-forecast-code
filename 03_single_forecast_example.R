ce31cf89c3557ed5581d59132d13c207206be950
rqthomas <rqthomas@vt.edu>
2021-08-03 08:57:21 -0400

    Fixing output path issue

--- 03_single_forecast_example.R
+++ 03_single_forecast_example.R
@@ -9,7 +9,7 @@ config$file_path$noaa_directory <- file.path(lake_directory, "data_processed","N
 config$file_path$configuration_directory <- file.path(lake_directory, "configuration")
 config$file_path$execute_directory <- file.path(lake_directory, "flare_tempdir")
 config$file_path$run_config <- file.path(paste0(lake_directory,"/configuration/", "FLAREr/", "run_configuration_",forecast_site,".yml"))
-config$file_path$forecast_output_directory <- file.path(lake_directory, "forecast_output")
+config$file_path$forecast_output_directory <- file.path(lake_directory, "forecast_output",forecast_site)
 config$run_config <- run_config
 
 
@@ -89,7 +89,7 @@ da_forecast_output <- FLAREr::run_da_forecast(states_init = init$states,
 
 print("Writing output file")
 saved_file <- FLAREr::write_forecast_netcdf(da_forecast_output = da_forecast_output,
-                                            forecast_output_directory = file.path(config$file_path$forecast_output_directory,forecast_site))
+                                            forecast_output_directory = file.path = config$file_path$forecast_output_directory)
 
 print("Creating metadata")
 FLAREr::create_flare_metadata(file_name = saved_file,
