cf43c224e8b13ccc2f6cf3b28c9b6c987edad048
rqthomas <rqthomas@vt.edu>
2021-10-20 06:22:05 -0400

    site id to download script

--- 01_downloads.R
+++ 01_downloads.R
@@ -7,6 +7,9 @@ setwd(lake_directory)
 source(file.path(lake_directory, "R/download_functions/NOAA_downloads.R"))
 source(file.path(lake_directory, "R/download_functions/NEON_downloads.R"))
 
+run_config <- yaml::read_yaml(file.path(paste0(lake_directory,"/configuration/", "FLAREr/", "run_configuration.yml")))
+forecast_site <- run_config$forecast_site
+
 buoy_products <- c("DP1.20264.001",
                    "DP1.20252.001",
                    "DP1.20254.001")
@@ -26,14 +29,14 @@ if(use_efi_server){
   date <- as.Date(run_config$forecast_start_datetime)
   cycle <- "00"
 
-  noaa_data_location <- file.path(dirname(lake_directory), "drivers","noaa","NOAAGEFS_1hr",siteID)
+  noaa_data_location <- file.path(dirname(lake_directory), "drivers","noaa","NOAAGEFS_1hr",forecast_site)
 
   for(p in 1:length(siteID)){
     for(i in 1:length(date)){
       for(g in 1:length(cycle)){
 
         if (length(list.files(file.path(noaa_data_location, date[i], cycle[g]))) != 31){
-          download_noaa_files_s3(siteID = siteID[p],
+          download_noaa_files_s3(siteID = forecast_site[p],
                                  date = date[i],
                                  cycle = cycle[g],
                                  noaa_directory = noaa_directory,
@@ -61,7 +64,7 @@ if (file.exists(file.path(neon_database))){
   neonstore::neon_dir()
 }
 
-download_neon_files(siteID = siteID,
+download_neon_files(siteID = forecast_site,
                     buoy_products = buoy_products,
                     start_date = as.Date("2021-01-01"),
                     raw_data_directory = raw_data_directory)
