3402ddbf214797eb54baf962be019fae9b8d7b70
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2022-12-05 12:02:10 -0500

    change model_id

--- workflows/default/challenge_submission.R
+++ workflows/default/challenge_submission.R
@@ -8,7 +8,13 @@ Sys.unsetenv("AWS_DEFAULT_REGION")
 Sys.unsetenv("AWS_S3_ENDPOINT")
 Sys.setenv(AWS_EC2_METADATA_DISABLED="TRUE")
 
-team_name <- 'FLARE'
+model_name <- 'flareGLM'
+
+NEON_sites <- readr::read_csv("https://raw.githubusercontent.com/eco4cast/neon4cast-targets/main/NEON_Field_Site_Metadata_20220412.csv") |> 
+  dplyr::filter(field_site_subtype == 'Lake') %>%
+  distinct(field_site_id) %>%
+  pull()
+
 
 # get the forecast from the FLARE bucket
 forecasts <- arrow::s3_bucket(bucket = "forecasts/parquet",
@@ -18,10 +24,7 @@ forecasts <- arrow::s3_bucket(bucket = "forecasts/parquet",
 today <- paste(Sys.Date(), '00:00:00')
 # yesterday <- paste((Sys.Date() - days(1)), '00:00:00')
 
-NEON_sites <- readr::read_csv("https://raw.githubusercontent.com/eco4cast/neon4cast-targets/main/NEON_Field_Site_Metadata_20220412.csv") |> 
-  dplyr::filter(field_site_subtype == 'Lake') %>%
-  distinct(field_site_id) %>%
-  pull()
+
 
 open_ds <- arrow::open_dataset(forecasts) %>%
   filter(site_id %in% NEON_sites, 
@@ -38,14 +41,14 @@ challenge_submission <- open_ds %>%
   group_by(site_id, datetime, parameter, reference_datetime,
            family, variable) %>%
   summarise(prediction = mean(prediction)) %>% 
-  mutate(model_id = 'FLARE', 
+  mutate(model_id = model_name, 
          reference_datetime = gsub(' 00:00:00', '', reference_datetime))%>%
   
   select(c('datetime', 'reference_datetime', 'site_id', 'family', 
                   'parameter', 'variable', 'prediction', 'model_id'))  
 
 # Write the submission
-forecast_file <- paste0('aquatics-', challenge_submission$reference_datetime[1], '-', team_name, '.csv.gz')
+forecast_file <- paste0('aquatics-', challenge_submission$reference_datetime[1], '-', model_name, '.csv.gz')
 
 write_csv(challenge_submission, forecast_file)
 # Submit forecast!
