db3754a94f2410a6f2abc95bc09cdfa3fb6c888b
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2022-12-05 12:16:39 -0500

    namespace functions

--- workflows/default/challenge_submission.R
+++ workflows/default/challenge_submission.R
@@ -12,8 +12,8 @@ model_name <- 'flareGLM'
 
 NEON_sites <- readr::read_csv("https://raw.githubusercontent.com/eco4cast/neon4cast-targets/main/NEON_Field_Site_Metadata_20220412.csv") |> 
   dplyr::filter(field_site_subtype == 'Lake') %>%
-  distinct(field_site_id) %>%
-  pull()
+  dplyr::distinct(field_site_id) %>%
+  dplyr::pull()
 
 
 # get the forecast from the FLARE bucket
@@ -27,35 +27,33 @@ today <- paste(Sys.Date(), '00:00:00')
 
 
 open_ds <- arrow::open_dataset(forecasts) %>%
-  filter(site_id %in% NEON_sites, 
-         reference_datetime == today,
-         forecast == 1,
-         depth <= 1) %>% 
-  collect() 
+  dplyr::filter(site_id %in% NEON_sites, 
+                reference_datetime == today,
+                datetime > as_datetime(reference_datetime),
+                depth <= 1) %>% 
+  dplyr::collect() 
 
 challenge_submission <- open_ds %>%
-  filter(variable == "temperature",
-         datetime >= today) %>%
+  dplyr::filter(variable == "temperature",
+                datetime >= today) %>%
   # FLARE output at multiple depths
   # Need a single "surface" average
-  group_by(site_id, datetime, parameter, reference_datetime,
+  dplyr::group_by(site_id, datetime, parameter, reference_datetime,
            family, variable) %>%
-  summarise(prediction = mean(prediction)) %>% 
-  mutate(model_id = model_name, 
-         reference_datetime = gsub(' 00:00:00', '', reference_datetime))%>%
+  dplyr::summarise(prediction = mean(prediction)) %>% 
+  dplyr::mutate(model_id = model_name, 
+                reference_datetime = gsub(' 00:00:00', '', reference_datetime))%>%
   
-  select(c('datetime', 'reference_datetime', 'site_id', 'family', 
+  dplyr::select(c('datetime', 'reference_datetime', 'site_id', 'family',
                   'parameter', 'variable', 'prediction', 'model_id'))  
 
 # Write the submission
 forecast_file <- paste0('aquatics-', challenge_submission$reference_datetime[1], '-', model_name, '.csv.gz')
 
-write_csv(challenge_submission, forecast_file)
+readr::write_csv(challenge_submission, forecast_file)
 # Submit forecast!
 
 # Now we can submit the forecast output to the Challenge using 
 neon4cast::forecast_output_validator(forecast_file)
 neon4cast::submit(forecast_file = forecast_file,
                   ask = F, s3_region = 'data', s3_endpoint = 'ecoforecast.org')
-
-
