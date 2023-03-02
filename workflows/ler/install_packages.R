1e62df2800fa9fc6e320f5a4c97b6fd72722cdb6
Freya Olsson <47249151+OlssonF@users.noreply.github.com>
2023-03-02 09:55:17 -0500

    Update install_packages.R

--- workflows/ler/install_packages.R
+++ workflows/ler/install_packages.R
@@ -1,6 +1,4 @@
-# install packages
-# install.packages(c("remotes", "tidyverse"))
-# install.packages(c("here", "aws.s3"))
+
 remotes::install_cran("xtable")
 remotes::install_github("GLEON/rLakeAnalyzer", ref = "e74974f74082111065bd9cd759527f16608b3c82")
 remotes::install_github("FLARE-forecast/GLM3r", ref = "6561ffd5ee5807dd1cac5cc9bb0fff7fc4c43b0b")
@@ -9,9 +7,7 @@ remotes::install_github("FLARE-forecast/SimstratR", ref = "c6ccb997705f700f6ff6c
 remotes::install_github("aemon-j/gotmtools", ref = "yaml")
 remotes::install_github("rqthomas/glmtools", ref = "b50e9a7b73e41afcd8119e2b9ac172c1c7beb51f")
 remotes::install_github("FLARE-forecast/LakeEnsemblR")
-#remotes::install_github("rqthomas/FLAREr", ref = "ca1fe5570d58a37e921ee6b0fe64c2947298fb87")
 remotes::install_github("FLARE-forecast/FLAREr")
-#remotes::install_github("FLARE-forecast/FLARErLER", ref = "b77740a3ba37487346d5b110c90fd92dfe7487d6")
-remotes::install_github("FLARE-forecast/FLARErLER")
+remotes::install_github("FLARE-forecast/FLARErLER", ref = "b77740a3ba37487346d5b110c90fd92dfe7487d6")
 
 # end
