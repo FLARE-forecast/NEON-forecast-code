8dcf6794cfa22923e3a51031503154f817bdf885
Quinn Thomas <rqthomas@vt.edu>
2023-03-13 20:14:09 -0400

    Update combined_workflow_BARC.R

--- workflows/default/combined_workflow_BARC.R
+++ workflows/default/combined_workflow_BARC.R
@@ -45,6 +45,8 @@ readr::read_csv("https://data.ecoforecast.org/neon4cast-targets/aquatics/aquatic
 noaa_ready <- TRUE
 while(noaa_ready){
   
+  config <- FLAREr::set_configuration(configure_run_file,lake_directory, config_set_name = config_set_name)
+  
   output <- FLAREr::run_flare(lake_directory = lake_directory,
                               configure_run_file = configure_run_file,
                               config_set_name = config_set_name)
