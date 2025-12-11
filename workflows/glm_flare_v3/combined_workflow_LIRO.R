c246b8ddf6598ede23557f8a13655ef9dc7bf6f3
Austin Delany <addelany@gmail.com>
2025-12-11 11:18:34 -0600

    Update combined_workflow_LIRO.R

--- workflows/glm_flare_v3/combined_workflow_LIRO.R
+++ workflows/glm_flare_v3/combined_workflow_LIRO.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(lubridate)
 
 remotes::install_github('flare-forecast/FLAREr')
-remotes::install_github("rqthomas/GLM3r@v3.1.0")
+remotes::install_github("rqthomas/GLM3r")
 remotes::install_github("cboettig/aws.s3")
 Sys.setenv('GLM_PATH'='GLM3r')
 
